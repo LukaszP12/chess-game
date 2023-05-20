bbf0981daace377661d8db91e2187de372c1e21d
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-21 00:09:36 +0200

    createAllPossibleEmptyTiles as HashMap

--- src/main/java/src/Tile.java
+++ src/main/java/src/Tile.java
@@ -1,8 +1,25 @@
 package src;
 
+import javax.management.ImmutableDescriptor;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Tile {
 
-    int tileCoordinate;
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
 
     public Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
@@ -14,7 +31,7 @@ public abstract class Tile {
 
     public static final class EmptyTile extends Tile {
 
-        EmptyTile(int coordinate) {
+        EmptyTile(final int coordinate) {
             super(coordinate);
         }
 
@@ -31,10 +48,10 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile {
 
-        Piece pieceOnTile;
+        private final Piece pieceOnTile;
 
-        public OccupiedTile(int coordinate, Piece pieceOnTile) {
-            super(coordinate);
+        public OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
 
