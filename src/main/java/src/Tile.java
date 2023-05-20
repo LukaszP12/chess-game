f85df94ac2110fa6e82649d9decfb9ab41db797e
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-20 23:30:02 +0200

    EmptyTile class as static belonging to Tile class

--- src/main/java/src/Tile.java
+++ src/main/java/src/Tile.java
@@ -12,4 +12,21 @@ public abstract class Tile {
 
     public abstract Piece getPiece();
 
+    public static final class EmptyTile extends Tile {
+
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
 }
