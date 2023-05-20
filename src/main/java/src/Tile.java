2e5b5a406156eae7aa0a16538439ad9587af6d28
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-20 23:23:45 +0200

    initial class indicating if a tile is occupied or not

---
+++ src/main/java/src/Tile.java
@@ -0,0 +1,15 @@
+package src;
+
+public abstract class Tile {
+
+    int tileCoordinate;
+
+    public Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+}
