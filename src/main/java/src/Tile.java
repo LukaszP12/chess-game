bab3c42afea1b7c1663193e73583f691c21eba9d
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-20 23:34:32 +0200

    OccupiedTile class as static belonging to Tile class

--- src/main/java/src/Tile.java
+++ src/main/java/src/Tile.java
@@ -29,4 +29,24 @@ public abstract class Tile {
         }
     }
 
+    public static final class OccupiedTile extends Tile {
+
+        Piece pieceOnTile;
+
+        public OccupiedTile(int coordinate, Piece pieceOnTile) {
+            super(coordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return pieceOnTile;
+        }
+    }
+
 }
