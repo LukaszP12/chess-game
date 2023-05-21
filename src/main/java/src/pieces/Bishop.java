bdbd049a4db069899da6cc339accfca445202553
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-22 00:16:53 +0200

    Bishop extending Piece

---
+++ src/main/java/src/pieces/Bishop.java
@@ -0,0 +1,55 @@
+package src.pieces;
+
+import src.Alliance;
+import src.board.Board;
+import src.board.BoardUtils;
+import src.board.Move;
+import src.board.Tile;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Bishop extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -7, 7, 9};
+
+    public Bishop(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES) {
+
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+                candidateDestinationCoordinate += currentCandidateOffset;
+
+                if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if (!candidateDestinationTile.isTileOccupied()) {
+                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                        if (this.pieceAlliance != pieceAlliance) {
+                            legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                    }
+                    break;
+
+                }
+            }
+        }
+
+        return Collections.unmodifiableList(legalMoves);
+    }
+}
