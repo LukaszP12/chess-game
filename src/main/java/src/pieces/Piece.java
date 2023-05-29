06e0bb377776a5162ef4fb5ab8839d5661ac5f33
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-05-29 15:38:20 +0200

    piece class and Alliance Part: II

--- src/main/java/src/pieces/Piece.java
+++ src/main/java/src/pieces/Piece.java
@@ -10,16 +10,22 @@ public abstract class Piece {
 
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
 
     public Piece(final int piecePosition, final Alliance pieceAlliance) {
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        this.isFirstMove = false;
     }
 
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
     }
 
-    public abstract Collection<Move> calculateMoves(final Board board);
+    public boolean isFirstMove() {
+        return this.isFirstMove;
+    }
+
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
 
 }
