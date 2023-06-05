c85b91303a6835537611c984563eb5ab66d03ab4
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-06-05 22:08:08 +0200

    added hashcode and equals implementations for the Piece class

--- src/main/java/src/pieces/Piece.java
+++ src/main/java/src/pieces/Piece.java
@@ -12,6 +12,7 @@ public abstract class Piece {
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
     protected final boolean isFirstMove;
+    private final int cachedHashCode;
 
     public Piece(final PieceType pieceType,
                  final Alliance pieceAlliance,
@@ -20,6 +21,37 @@ public abstract class Piece {
         this.pieceAlliance = pieceAlliance;
         this.piecePosition = piecePosition;
         this.isFirstMove = false;
+        this.cachedHashCode = computedHashCode();
+    }
+
+    private int computedHashCode() {
+        int result = pieceType.hashCode();
+        result = 31 * result + pieceAlliance.hashCode();
+        result = 31 * result + piecePosition;
+        result = 31 * result + (isFirstMove ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Piece)) {
+            return false;
+        }
+        final Piece otherPiece = (Piece) other;
+        return piecePosition == otherPiece.getPiecePosition() && pieceType == otherPiece.getPieceType() &&
+                pieceAlliance == otherPiece.getPieceAlliance() && isFirstMove == otherPiece.isFirstMove();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.cachedHashCode;
+    }
+
+    public int getPiecePosition() {
+        return piecePosition;
     }
 
     public Alliance getPieceAlliance() {
@@ -30,17 +62,13 @@ public abstract class Piece {
         return this.isFirstMove;
     }
 
-    public int getPiecePosition() {
-        return piecePosition;
+    public PieceType getPieceType() {
+        return pieceType;
     }
 
     public abstract Collection<Move> calculateLegalMoves(final Board board);
 
     public abstract Piece movePiece(Move move);
-
-    public PieceType getPieceType() {
-        return pieceType;
-    }
 
     public enum PieceType {
 
